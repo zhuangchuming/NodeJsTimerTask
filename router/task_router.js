f710a368d0051812cbc876a516ccc0c7f1a94608
zhuangchuming <522236495@qq.com>
2016-11-30 15:16:00 +0800

    增加跨域访问

--- router/task_router.js
+++ router/task_router.js
@@ -7,6 +7,16 @@ var url = require('url');
 var taskDao = require('../dao/taskDao.js');
 var router = global.router;//获取路由
 
+//设置跨域访问
+router.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
 router.get('/',function (request,response) {
     // 解析请求，包括文件名
     var pathname = url.parse(request.url).pathname;
